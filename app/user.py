b73565cc18b7b40d39e1ac4fe829e68f000c5d4d
kabirajrana <199759567+kabirajrana@users.noreply.github.com>
2026-06-22 21:55:39 +0545

    add the user routs

---
+++ app/user.py
@@ -0,0 +1,10 @@
+# Example SQLAlchemy model (optional)
+from sqlalchemy import Column, Integer, String
+from app.db.session import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
